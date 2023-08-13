53b7492d2530a185b1150faaee8ab7c14b5447d7
SrujanaAddi <125382455+SrujanaAddi@users.noreply.github.com>
2023-08-13 19:12:58 +0530

    validations added

--- src/main/java/com/fms/airport/DTO/AirportDTO.java
+++ src/main/java/com/fms/airport/DTO/AirportDTO.java
@@ -1,10 +1,24 @@
 package com.fms.airport.DTO;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class AirportDTO {
 	
+	
 	Integer airportId;
+	
+	@NotBlank(message = "airport name cannot be blank")
+	@Pattern(regexp = "^[a-zA-Z '-]*$", message = "Input should not contain special characters or numbers")
+	@Size(min = 3, max = 30, message = "Input length should be between 3 and 30 characters")
 	String airportName;
+	
+	@NotBlank(message = "airport name cannot be blank")
+	@Pattern(regexp = "^[a-zA-Z '-]*$", message = "Input should not contain special characters or numbers")
+	@Size(min = 3, max = 30, message = "Input length should be between 3 and 30 characters")
 	String airportLocation;
+	
 	public Integer getAirportId() {
 		return airportId;
 	}
